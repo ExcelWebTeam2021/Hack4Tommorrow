21963e8d31b677e1ebb57ad5f661169f432a4f5a
annu12340 <annujolly17@gmail.com>
2021-09-30 13:44:16 +0530

    Refactored the code

--- assets/js/genderequality.js
+++ assets/js/genderequality.js
@@ -1,6 +1,6 @@
 ScrollLottie({
-    target: "#landing",
-    path: 'https://assets10.lottiefiles.com/packages/lf20_yld1s2nm.json',
-    duration: 2,
-    speed: 'medium'
-})
+   target: "#landing",
+   path: "https://assets5.lottiefiles.com/packages/lf20_b4lnka8w.json",
+   duration: 2,
+   speed: "medium",
+});
