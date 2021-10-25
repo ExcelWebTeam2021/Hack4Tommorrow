8f71a6a6da8013a18e40691255a1f27d48240e9d
annu12340 <annujolly17@gmail.com>
2021-10-25 22:12:16 +0530

    Fixed some UI bugs

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,27 +1,57 @@
-ScrollLottie({
-   target: "#initial_idea",
-   path: "assets/animation/initial_idea.json",
-   duration: 1,
-   speed: "medium",
-});
+if ($(window).width() <= 800) {
+   ScrollLottie({
+      target: "#initial_idea",
+      path: "assets/animation/initial_idea.json",
+      duration: 1,
+      speed: "fast",
+   });
 
-ScrollLottie({
-   target: "#execution",
-   path: "assets/animation/idea_execution.json",
-   duration: 1,
-   speed: "medium",
-});
+   ScrollLottie({
+      target: "#execution",
+      path: "assets/animation/idea_execution.json",
+      duration: 1,
+      speed: "fast",
+   });
 
-ScrollLottie({
-   target: "#hackathon_rewards",
-   path: "assets/animation/rewards.json",
-   duration: 1,
-   speed: "fast",
-});
+   ScrollLottie({
+      target: "#hackathon_rewards",
+      path: "assets/animation/rewards.json",
+      duration: 1,
+      speed: "fast",
+   });
 
-ScrollLottie({
-   target: "#showcase",
-   path: "assets/animation/showcase.json",
-   duration: 1,
-   speed: "medium",
-});
+   ScrollLottie({
+      target: "#showcase",
+      path: "assets/animation/showcase.json",
+      duration: 1,
+      speed: "fast",
+   });
+} else {
+   ScrollLottie({
+      target: "#initial_idea",
+      path: "assets/animation/initial_idea.json",
+      duration: 1,
+      speed: "medium",
+   });
+
+   ScrollLottie({
+      target: "#execution",
+      path: "assets/animation/idea_execution.json",
+      duration: 1,
+      speed: "medium",
+   });
+
+   ScrollLottie({
+      target: "#hackathon_rewards",
+      path: "assets/animation/rewards.json",
+      duration: 1,
+      speed: "medium",
+   });
+
+   ScrollLottie({
+      target: "#showcase",
+      path: "assets/animation/showcase.json",
+      duration: 1,
+      speed: "medium",
+   });
+}
